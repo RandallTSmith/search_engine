a5f1cd98bbac2aeec74473e312577af12ed5c5bb
ZoeStarace <zoe.starace@gmail.com>
2025-05-30 20:22:27 +0000

    Update data frame to read in test csv vs hard coded examples

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,28 +17,9 @@ st.write(
 # reruns (e.g. if the user interacts with the widgets).
 @st.cache_data
 def load_data():
-    data = {'CLAIM_NO': [1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 
-        'CLAIM_TYPE': ['CLAIM', 'SUIT', 'ALERT', 'CLAIM', 'SUIT', 'SUIT', 'ALERT', 'CLAIM', 'SUIT', 'SUIT'], 
-        'LOSS_TYPE': ['PROF LIAB', 'GL', 'PROF LIAB', 'GL', 'PROF LIAB', 'PROF LIAB',  'PROF LIAB', 'GL', 'PROF LIAB', 'PROF LIAB'], 
-        'AGENCY_ID': ["001", "002", "003", "001", "002", "003", "001", "002", "003", "001"], 
-        'VAP_HOSP_INDICATOR': [1, 1, 0, 1, 1, 0, 1, 1, 1, 0], 
-        'LOSS_DATE': ['2022-01-01', '2022-02-01', '2022-03-01', '2023-01-01', '2023-02-01', '2023-03-01', '2024-03-01', '2024-01-01', '2024-02-01', '2025-03-01'], 
-        'ASSERTED_DATE': ['2023-01-01', '2023-02-01', '2023-03-01', '2023-01-01', '2024-02-01', '2024-03-01', '2024-03-01', '2025-01-01', '2025-02-01', '2025-03-01'], 
-        'TOTAL_INCURRED': [1000, 2000, 3000, 4000, 5000, 6000, 7000, 8000, 9000, 10000], 
-        'NOTE_CREATION_DATE': ['2023-01-01', '2023-02-01', '2023-03-01', '2023-01-01', '2024-02-01', '2024-03-01', '2024-03-01', '2025-01-01', '2025-02-01', '2025-03-01'], 
-        'NOTE_TYPE': ['EXPERT', 'REPORT', 'EXPERT', 'EXPERT', 'REPORT', 'EXPERT', 'EXPERT', 'REPORT', 'EXPERT', 'REPORT'], 
-        'NOTE_DESCRIPTION': ['The fox jumped over the cat, and the cat ran away.', 
-                             'Jack and Jill ran up the hill to fetch a cat.', 
-                             'The fox lives in a hole on  a hill with his pet rabbit, Joe. Joe is 10 years old.',
-                             'Jack and Jill are guilty of theft.',
-                             'The fox is a very clever animal. That is a pathologist',
-                             'Tim lives in England and he plays cricket on the weekends.',
-                             'Shelby is a oncologist who treats patients with cancer.',
-                             'Failure to diagnose fox in a 62 year old male with a history of hypertension.',
-                             'Some foxes have failure to play cricket',
-                             'Coding is fun as long as you are not a fox.']}
-    df = pd.DataFrame(data)
-    #manipulate data
+    # Replace 'your_data.csv' with the path to your CSV file
+    df = pd.read_csv('test_claim_data.csv')
+    # manipulate data
     df['ASSERTED_YEAR'] = pd.DatetimeIndex(df['ASSERTED_DATE']).year
     return df
 
@@ -55,7 +36,7 @@ claim_type = st.multiselect(
 loss_type = st.multiselect(
     "Loss Type",
     df.LOSS_TYPE.unique(),
-    ["PROF LIAB", "GL"],
+    ["PROF LIAB", "GEN LIAB", "ADMIN"],
 )
 
 # Show a slider widget with the years using `st.slider`.
@@ -63,7 +44,7 @@ years = st.slider("Years", 1995, 2006, (2020, 2025))
 
 # Filter the dataframe based on the widget input and reshape it.
 df_filtered = df[(df["CLAIM_TYPE"].isin(claim_type)) & (df["LOSS_TYPE"].isin(loss_type)) & (df["ASSERTED_YEAR"].between(years[0], years[1]))]
-df_reshaped = df_filtered[['CLAIM_NO','ASSERTED_YEAR','TOTAL_INCURRED','NOTE_TYPE','NOTE_DESCRIPTION']]
+df_reshaped = df_filtered[['CLAIM_NUMBER','ASSERTED_YEAR','TOTAL_INCURRED','NOTE_TYPE','NOTE_DESCRIPTION']]
     #.pivot_table(
     #index="ASSERTED_YEAR", columns="CLAIM_TYPE", values="TOTAL_INCURRED", aggfunc="sum", fill_value=0
     #)
